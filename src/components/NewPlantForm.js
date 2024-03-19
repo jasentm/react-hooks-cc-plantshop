71b2e92eacd1487a4530b36b04854f9792a92bd3
Jasen <jasen.miyamoto@gmail.com>
2024-03-19 15:44:26 -0400

    core deliverables working, need comments

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({addNewPlant}) {
+  const initialForm = {
+    name: '',
+    image: '',
+    price: ''
+  }
+  const [form, setForm] = useState(initialForm)
+  
+  const handleFormChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handlePlantSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {'Content-Type': "application/json"},
+      body: JSON.stringify({
+        name: form.name,
+        image: form.image,
+        price: parseInt(form.price)
+      })
+    })
+    .then(res => {
+      if(res.ok){
+        return (res.json())
+      }else{
+        return (console.error('Something went wrong...'))
+      }
+    })
+    .then(newPlantData => {
+      addNewPlant(newPlantData)
+      setForm(initialForm)
+    })
+    .catch(error => console.error(error))
+    }
+  
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handlePlantSubmit}>
+        <input 
+          type="text" 
+          name="name" 
+          placeholder="Plant name" 
+          onChange={handleFormChange} 
+          value={form.name}
+        />
+        <input 
+          type="text" 
+          name="image" 
+          placeholder="Image URL" 
+          onChange={handleFormChange} 
+          value={form.image}
+        />
+        <input 
+          type="number" 
+          name="price" 
+          step="0.01" 
+          placeholder="Price"
+          onChange={handleFormChange} 
+          value={form.price} 
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState('')
+  
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => {
+      if(res.ok){
+        return (res.json())
+      }else{
+        return (console.error("Something went wrong..."))
+      }
+    })
+    .then(plantData => setPlants(plantData))
+  }, [])
+
+  const addNewPlant = (newPlant) => {
+    setPlants([...plants, newPlant])
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+  
+  const filteredPlants = plants.filter(plant => {
+    if(plant.name.toLowerCase().includes(search.toLowerCase())){
+      return true
+    }else {
+      return false
+    }
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search search={search} handleSearch={handleSearch}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
